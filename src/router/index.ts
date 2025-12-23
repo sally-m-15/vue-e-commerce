17bf1d496732d97c1b6131e5aaa798ef07be88e1
sally-m-15 <sallymohammed1521999@gmail.com>
2025-12-23 15:33:26 +0200

    Fix router for GitHub Pages (hash mode)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { routes } from "./guards";
 import { useAuthStore } from "@/composables/services/useAuthApi";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+   history: createWebHashHistory(),
   routes,
 });
 
