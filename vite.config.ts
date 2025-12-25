7876ab2305dc914203ac766f6813e11d6d3eb1fd
sally-m-15 <sallymohammed1521999@gmail.com>
2025-12-25 12:49:33 +0200

    fix: change base path for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/vue-e-commerce/',
-  plugins: [
-    
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+  base: "/",
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
     },
   },
-})
+});
