ff9760e6a2f8d257c00942ec2fdd801701fa1805
sally-m-15 <sallymohammed1521999@gmail.com>
2026-01-13 06:43:40 +0200

    optimizing preconnect

--- src/pages/Shopping/Products.vue
+++ src/pages/Shopping/Products.vue
@@ -1,16 +1,26 @@
 <template>
     <section class="pt-28">
-      <SearchBar />
-      <HomeProducts />
+            <div class="min-h-[200px]">
+        <SearchBar />
+      </div>
+      <div v-if="allProducts.isLoading" class="flex justify-center py-20 min-h-screen">
+        <vue-spinner-clip :loading="true" :color="'#16a34a'" :size="50" />
+      </div>
+      
+      <HomeProducts v-show="allProducts.products.length > 0" /> 
     </section>
   <router-view />
 </template>
 
 <script setup lang="ts">
+import { allProductsStore } from "@/api/products";
 import { defineAsyncComponent } from "vue";
 
 const HomeProducts = defineAsyncComponent(()=> import("@components/home/HomeProducts.vue"));
-const SearchBar = defineAsyncComponent(()=> import("@components/home/SearchBar.vue"))
+const SearchBar = defineAsyncComponent(()=> import("@components/home/SearchBar.vue"));
+
+const allProducts = allProductsStore();
+
 </script>
 
 <style scoped></style>
